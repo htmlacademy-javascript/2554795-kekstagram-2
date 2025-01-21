d9648093d3afcfb2d83e6301a0f0ee07edf05861
barmale1 <barmale1@proton.me>
2025-01-21 09:23:23 +0300

    pictures

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {getPhoto} from './data.js';
+import {renderPhotos} from './picture.js';
 
 const PHOTOS_MAX = 25;
 
 const photos = Array.from({length: PHOTOS_MAX}, getPhoto);
+renderPhotos(photos);
